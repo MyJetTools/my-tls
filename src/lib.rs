3858cf4ddc2911cc60203b1501112bd9eb9c2e27
amigin <amigin@gmail.com>
2024-07-11 20:28:19 +0300

    Made possible to create certs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 mod client_certificate;
-use std::sync::Arc;
 
 pub use client_certificate::*;
 mod cert_content;
@@ -7,7 +6,7 @@ pub use cert_content::*;
 mod certs_iterator;
 pub use certs_iterator::*;
 
-pub fn get_trusted_certs(other_certs: &[u8]) -> Arc<tokio_rustls::rustls::RootCertStore> {
+pub fn get_trusted_certs(other_certs: Option<&[u8]>) -> tokio_rustls::rustls::RootCertStore {
     let mut root_cert_store = tokio_rustls::rustls::RootCertStore::empty();
 
     let certificates_iterator = CertificatesIterator::new(ALL_CERTIFICATES);
@@ -18,12 +17,14 @@ pub fn get_trusted_certs(other_certs: &[u8]) -> Arc<tokio_rustls::rustls::RootCe
             .map(|itm| pem_to_der(&ALL_CERTIFICATES[itm.0..itm.1])),
     );
 
-    let certificates_iterator = CertificatesIterator::new(other_certs);
-    root_cert_store.add_parsable_certificates(
-        certificates_iterator
-            .into_iter()
-            .map(|itm| pem_to_der(&other_certs[itm.0..itm.1])),
-    );
+    if let Some(other_certs) = other_certs {
+        let certificates_iterator = CertificatesIterator::new(other_certs);
+        root_cert_store.add_parsable_certificates(
+            certificates_iterator
+                .into_iter()
+                .map(|itm| pem_to_der(&other_certs[itm.0..itm.1])),
+        );
+    }
 
-    Arc::new(root_cert_store)
+    root_cert_store
 }
