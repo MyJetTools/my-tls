752c1a3f9364996271cfc180789cbc687e9935bf
amigin <amigin@gmail.com>
2024-08-17 23:08:36 +0300

    Added EmptyCRL

--- src/crl.rs
+++ src/crl.rs
@@ -30,15 +30,32 @@ pub fn read(src: &[u8]) -> Result<Vec<CrlRecord>, String> {
     Ok(result)
 }
 
-/*
+pub const EMPTY_CRL: &str = r#"
+-----BEGIN X509 CRL-----
+MIICdzBhAgEBMA0GCSqGSIb3DQEBBQUAMB8xHTAbBgNVBAMMFFB1cHBldCBDQTog
+bG9jYWxob3N0Fw0xMzA3MTYyMDQ4NDJaFw0xODA3MTUyMDQ4NDNaoA4wDDAKBgNV
+HRQEAwIBADANBgkqhkiG9w0BAQUFAAOCAgEAqyBJOy3dtCOcrb0Fu7ZOOiDQnarg
+IzXUV/ug1dauPEVyURLNNr+CJrr89QZnU/71lqgpWTN/J47mO/lffMSPjmINE+ng
+XzOffm0qCG2+gNyaOBOdEmQTLdHPIXvcm7T+wEqc7XFW2tjEdpEubZgweruU/+DB
+RX6/PhFbalQ0bKcMeFLzLAD4mmtBaQCJISmUUFWx1pyCS6pgBtQ1bNy3PJPN2PNW
+YpDf3DNZ16vrAJ4a4SzXLXCoONw0MGxZcS6/hctJ75Vz+dTMrArKwckytWgQS/5e
+c/1/wlMZn4xlho+EcIPMPfCB5hW1qzGU2WjUakTVxzF4goamnfFuKbHKEoXVOo9C
+3dEQ9un4Uyd1xHxj8WvQck79In5/S2l9hdqp4eud4BaYB6tNRKxlUntSCvCNriR2
+wrDNsMuQ5+KJReG51vM0OzzKmlScgIHaqbVeNFZI9X6TpsO2bLEZX2xyqKw4xrre
+OIEZRoJrmX3VQ/4u9hj14Qbt72/khYo6z/Fckc5zVD+dW4fjP2ztVTSPzBqIK3+H
+zAgewYW6cJ6Aan8GSl3IfRqj6WlOubWj8Gr1U0dOE7SkBX6w/X61uqsHrOyg/E/Z
+0Wcz/V+W5iZxa4Spm0x4sfpNzf/bNmjTe4M2MXyn/hXx5MdHf/HZdhOs/lzwKUGL
+kEwcy38d6hYtUjs=
+-----END X509 CRL-----
+"#;
+
 #[cfg(test)]
 mod tests {
 
     #[test]
     fn test_crl_reading() {
-        let records = super::read(super::TEST_CLR).unwrap();
+        let records = super::read(super::EMPTY_CRL.as_bytes()).unwrap();
 
         println!("Records: {:#?}", records);
     }
 }
- */
